e34bba9a804feca3bb0015297c9d91dea34d1ed5
Emmanuel <oladipoemmanuel453@gmail.com>
2025-06-19 23:08:19 +0100

    BCC Gallery

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1543,7 +1543,7 @@ aria-label="Scroll to top"
                         </button>
                     </div>
                     <hr />
-                    <p style={{ color: "#6b7280", marginBottom: "0.60rem", marginTop: "1rem", textAlign: "center" }}>
+                    <p style={{ color: "#6b7280", marginBottom: "0.60rem", marginTop: "1.125rem", textAlign: "center" }}>
   Found {galleryImages.length} image{galleryImages.length !== 1 ? "s" : ""}
 </p>
                     {isLoadingGallery ? (
