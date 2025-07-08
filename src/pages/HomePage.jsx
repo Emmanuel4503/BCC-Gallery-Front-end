41f1ca145c7ac41923895723cd4006c5ebd9769e
Emmanuel <oladipoemmanuel453@gmail.com>
2025-07-08 19:40:56 +0100

    BCC Gallery

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -333,7 +333,7 @@ const fetchGalleryImages = async (silent = false) => {
 
 const fetchUserReactions = async (userId) => {
   try {
-    const response = await fetch(`https://bcc-gallery-back-end-production.up.railway.app/reactions/user/${userId}`, {
+    const response = await fetch(`https://bcc-gallery-back-end-production.up.railway.app/reactions/${userId}`, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
