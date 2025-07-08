d4891bfdef35659fedd1b0c1357fc2c959e5a0fd
Emmanuel <oladipoemmanuel453@gmail.com>
2025-07-08 10:47:56 +0100

    BCC Gallery

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1615,7 +1615,7 @@ aria-label="Scroll to top"
                         </button>
                         <button onClick={openGallerySlideshow} className="action-btn view-gallery">
   <ArrowUp className="btn-icon" />
-  View Gallery (Found {galleryImages.length} images)
+  View Gallery (Total: {galleryImages.length} images)
 </button>
                     </div>
                     <hr />
